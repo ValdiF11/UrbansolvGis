eb1c3ceecc572b84eb92f2d37751b0d0535aa19d
ValdiF11 <valdifirstianto@gmail.com>
2024-06-20 21:14:05 +0700

    feat: initial setup backend server

---
+++ Server/models/user.js
@@ -0,0 +1,98 @@
+"use strict";
+const { Model } = require("sequelize");
+const { hashPassword } = require("../helper/bycript");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      User.hasMany(models.Route, {
+        foreignKey: "UserId",
+      });
+    }
+  }
+  User.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Name is required",
+          },
+          notNull: {
+            msg: "Name is required",
+          },
+        },
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: {
+          args: true,
+          msg: "Username is already taken",
+        },
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Username is required",
+          },
+          notNull: {
+            msg: "Username is required",
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: {
+          args: true,
+          msg: "Email is already taken",
+        },
+        allowNull: false,
+        validate: {
+          isEmail: {
+            msg: "Invalid email format",
+          },
+          notEmpty: {
+            msg: "Email is required",
+          },
+          notNull: {
+            msg: "Email is required",
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Password is required",
+          },
+          notNull: {
+            msg: "Password is required",
+          },
+          len: {
+            args: [8, 255],
+            msg: "Password must be at least 8 characters",
+          },
+        },
+      },
+      role: {
+        type: DataTypes.STRING,
+        defaultValue: "user",
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      hooks: {
+        beforeCreate: (user) => {
+          user.password = hashPassword(user.password);
+        },
+      },
+    }
+  );
+  return User;
+};
